0033ec93e2eaa0aa8f7efaa1f4711decb9a9a6b4
michelroger <matrix_a3@hotmail.com>
2026-04-06 10:02:02 -0300

    correções no hinário

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* PWA service worker (simples) */
-const CACHE_VERSION = 'v50';
+const CACHE_VERSION = 'v55';
 const CACHE_NAME = 'gem-tools-' + CACHE_VERSION;
 
 // Arquivos que fazem sentido precachear (single-file app).
